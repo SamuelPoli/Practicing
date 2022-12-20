663526128c0acc851e03fb3535e5967bd1ab51c0
SamuelPoli <sagamarra@poligran.edu.co>
2022-12-20 13:05:50 -0500

    Uso Static

--- Java/Uso_empleado.java
+++ Java/Uso_empleado.java
@@ -5,13 +5,13 @@ import java.util.*;
 public class Uso_empleado {
     public static void main(String[] args) {
         
-        empleado empleado1 = new empleado("Paco Gomez", 85000, 1990, 12, 17);
+        /*empleado empleado1 = new empleado("Paco Gomez", 85000, 1990, 12, 17);
         empleado empleado2 = new empleado("Ana Lopez", 95000, 1995, 6, 2);
         empleado empleado3 = new empleado("Maria Martin", 105000, 2002, 3, 15);
 
         empleado1.subesueldo(5);
-        empleado2.subesueldo(5.5);
-        empleado3.subesueldo(12.3);
+        empleado2.subesueldo(5);
+        empleado3.subesueldo(5);
 
         System.out.println("Nombre: "+empleado1.getNombre() + " Sueldo: "+empleado1.getSueldo()
         + " Fecha de alta: "+empleado1.getAltaContrato());
@@ -21,28 +21,44 @@ public class Uso_empleado {
 
         System.out.println("Nombre: "+empleado3.getNombre() + " Sueldo: "+empleado3.getSueldo()
         + " Fecha de alta: "+empleado3.getAltaContrato());
+        */
+
+        empleado[] misEmpleados = new empleado[3];
+
+        misEmpleados[0]= new empleado("Paco Gomez", 85000, 1990, 12, 17);
+        misEmpleados[1]= new empleado("Ana Lopez", 95000, 1995, 6, 2);
+        misEmpleados[2]= new empleado("Maria Martin", 105000, 2002, 3, 15);
+        
+        for (empleado e : misEmpleados) {
+            e.subesueldo(5);
+            System.out.println("ID: "+ e.getId()+" Nombre: "+e.getNombre() + " Sueldo: "+e.getSueldo()
+            + " Fecha de alta: "+e.getAltaContrato());
+        }
+
+        System.out.println(empleado.getIdSiguiente());
+            
     }
 }
 
 class empleado{
 
-    private String nombre;
+    private final String nombre;
     private double sueldo;
     private Date altaContrato;
+    private static int IdSiguiente = 1;
+    private int Id;
 
     public empleado(String nom, double sue, int ano, int mes, int dia){
         nombre=nom;
         sueldo=sue;
         GregorianCalendar calendario = new GregorianCalendar(ano, mes-1, dia);//Mes empieza en 0
         altaContrato=calendario.getTime();
+        Id=IdSiguiente;
+        IdSiguiente++;
     }
 
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public double getSueldo() {
@@ -64,5 +80,13 @@ class empleado{
     public void subesueldo(double porcentaje){
         double aumento = this.sueldo*porcentaje/100;
         this.sueldo+=aumento;
+    }
+
+    public int getId(){
+        return Id;
+    }
+
+    public static String getIdSiguiente(){
+        return "El Id siguiente es: "+IdSiguiente;
     }
 }
