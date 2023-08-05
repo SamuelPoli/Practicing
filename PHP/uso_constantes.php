42f7231b44c781500be36bf56dbd3f827bf14c4f
SamuelPoli <93875752+SamuelPoli@users.noreply.github.com>
2023-08-05 18:11:44 -0500

    Create uso_constantes.php

---
+++ PHP/uso_constantes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+define("AUTOR", "Samuel");
+
+echo "La linea de esta instruccion es: " . __LINE__ . "<br>";
+echo "Estamos trabajando en el fichero: " . __FILE__ . "<br>";
+echo "Autor: " . AUTOR;
+
+
+
+?>
+</body>
+</html>
